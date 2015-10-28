ce01686e0739e76fe54fa8c788b021145f76911b
David Dominguez <d2harker@gmail.com>
2015-10-28 17:56:56 -0400

    authendpoints to take in client's email

--- src/authendpoints.js
+++ src/authendpoints.js
@@ -1,9 +1,16 @@
-export const optionsLogin = {
-  url: 'http://localhost:8000/client/get/sathyp@rpi.edu',
-  headers: {
-    'User-Agent': 'request'
-  }
-};
+/**
+* @api optionsLogin
+* @apiGroup Client
+*
+* @apiParam {String} email Client's email
+*
+*/
+export const optionsLogin = (email) => ({
+    url: 'http://localhost:8000/client/get/' + email,
+    headers: {
+      'User-Agent': 'request'
+    }
+  });
 
 export const optionsRegister = {
   url: 'http://localhost:8000/client/create',
